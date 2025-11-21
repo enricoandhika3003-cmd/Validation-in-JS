00903e31d13cdcd8430be4ef10ca734b9716b019
enricoandhika3003-cmd <enrico.andhika3003@gmail.com>
2025-11-21 18:51:37 +0700

    done 2

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 function validateForm() {
-    var x = document.forms["Forms"]["fname"].value;
+    var x = document.forms["First"]["fname"].value;
     if (x == "") {
         alert("PLEASE INSERT YOUR NAME!!");
         return false;
     }
     return true;
 }
+
+
+const form = document.getElementById("APIForms");
+const message = document.getElementById("message");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    if (form.checkValidity()) {
+        message.style.color = "green";
+        message.textContent = "Thanks for signing up! 👍";
+    }
+    else {
+        message.style.color = "red";
+        message.textContent = "⚠️ Please fill out the fields correctly! ⚠️";
+    }
+})
